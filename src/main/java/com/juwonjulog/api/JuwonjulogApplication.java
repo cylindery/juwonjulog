9b2d927c5b7b518135f17f8b72d1a3d5715bb372
juwonju <jxwxnjx@gmail.com>
2023-09-06 19:16:26 +0900

    create project

---
+++ src/main/java/com/juwonjulog/api/JuwonjulogApplication.java
@@ -0,0 +1,13 @@
+package com.juwonjulog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuwonjulogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuwonjulogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juwonjulog/api/JuwonjulogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juwonjulog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuwonjulogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
