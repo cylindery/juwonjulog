0b64dd47dcdc61629ee0eb99d3d0a51b147220cb
juwonju <jxwxnjx@gmail.com>
2023-09-06 21:56:53 +0900

    feat: add post controller

--- src/main/java/com/juwonjulog/api/controller/PostController.java
+++ src/main/java/com/juwonjulog/api/controller/PostController.java
@@ -1,13 +1,21 @@
 package com.juwonjulog.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.juwonjulog.api.controller.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class PostController {
 
-    @GetMapping("/posts")
-    public String get() {
+    // 글 등록
+    // POST Method
+    @PostMapping("/posts")
+    public String post(@RequestBody PostCreate params) {
+        log.info("params={}", params);
         return "Hello World";
     }
+
 }
--- src/test/java/com/juwonjulog/api/controller/PostControllerTest.java
+++ src/test/java/com/juwonjulog/api/controller/PostControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -18,9 +19,12 @@ class PostControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("/posts 요청 시 Hello World를 출력한다.")
-    void get() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+    @DisplayName("/posts에 POST 요청 시 Hello World를 출력한다.")
+    void post() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"글 제목\", \"content\": \"글 내용...\"}")
+                )
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World"))
                 .andDo(print());
