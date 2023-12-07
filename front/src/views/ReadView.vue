48624df16d548d68b6452c4415dccee0ab6a7372
cylindery <cylindery0@gmail.com>
2023-12-07 19:34:02 +0900

    feat: create post edit view

--- front/src/views/ReadView.vue
+++ front/src/views/ReadView.vue
@@ -2,6 +2,7 @@
 
 import {onMounted, ref} from "vue";
 import axios from "axios";
+import {useRouter} from "vue-router";
 
 const props = defineProps({
   postId: {
@@ -21,9 +22,17 @@ onMounted(() => {
     post.value = response.data
   })
 })
+
+const router = useRouter()
+
+const moveToEdit = () => {
+  router.push({name: 'edit', params: {postId: props.postId}})
+}
 </script>
 
 <template>
   <h2>{{ post.title }}</h2>
   <div>{{ post.content }}</div>
+
+  <el-button type="warning" @click="moveToEdit()">글 수정</el-button>
 </template>
